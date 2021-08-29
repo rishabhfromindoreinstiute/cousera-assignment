e9e8fa1df665504a9de61a4535698d6c84d5137b
rishabhfromindoreinstitute <rishabh.khandagrecs2020@indoreinstitute.com>
2021-08-29 16:35:52 +0530

    assignment

---
+++ Module 4solution/Goodbyescript.js
@@ -0,0 +1,8 @@
+(function (window){
+	var byeSpeaker={};
+	byeSpeaker.speak=function(){
+		console.log(speakWord+" "names[i]);
+	}
+	 var speakWord = "Goodbye";
+	 window.byeSpeaker=byeSpeaker;
+})(window);
---
+++ Module 4solution/Helloscript.js
@@ -0,0 +1,8 @@
+(function (window){
+	var helloSpeaker={};
+	helloSpeaker.speak=function(){
+		console.log(speakWord+" "names[i]);
+	}
+	 var speakWord = "Hello";
+	 window.helloSpeaker=helloSpeaker;
+})(window);
